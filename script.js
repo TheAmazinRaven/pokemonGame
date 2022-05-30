0f67a5a37c08136c6cca707cc175510d1aef4994
Raven <raejdickerson@gmail.com>
2022-05-30 15:56:07 -0500

    Implemented classes in projcet.

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 // Welcome to the game! :D
 var battle = window.prompt("Would you like to play against Pikachu? \n [1] - Yes \n [2] - No");
 
+class Pokemon {
+    constructor(name, HP, powerMove, damage, potion) {
+        this.name = name;
+        this.HP = HP;
+        this.powerMove = powerMove;
+        this.damage = damage;
+        this.potion = potion;
+
+        // If player selects a Pokemon, allow greeting for that Pokemon to play
+
+        console.log(`${name} has ${HP} HP, their power move is ${powerMove}, it does ${damage} damage and they have a chance of healing by ${potion}%!`);
+    }
+    attack(otherPokemon) {
+        otherPokemon.HP = otherPokemon.HP - this.damage;
+        console.log(`${this.name} attacked ${otherPokemon.name} and did ${this.damage}! ${otherPokemon.name} is now at ${otherPokemon.HP}!`);
+    }
+}
+
+let myPokemon;
 
 for (let i = 0; i < 5; i++) {
 
@@ -18,12 +37,15 @@ for (let i = 0; i < 5; i++) {
             if (select === "1") {
                 let pokemon = "Charmander";
                 console.log("You've selected Charmander!");
+                myPokemon = new Pokemon("Charmander", 15, "Ember", 4, 1.5);
             } else if (select === "2") {
                 let pokemon = "Bulbasaur";
                 console.log("You've selected Bulbasaur!");
+                myPokemon = new Pokemon("Bulbasaur", 14, "Vine Whip", 4.5,2);
             } else if (select === "3") {
                 let pokemon = "Squirtle";
                 console.log("You've selected Squirtle!");
+                myPokemon = new Pokemon("Squirtle", 13, "Water Gun", 5, 3);
             }
 
     } else {
@@ -33,47 +55,34 @@ for (let i = 0; i < 5; i++) {
 
 }
 
-let PikachuHP = 17;
-let PikachuAttack = 5;
-let PikachuPotion = 2;
+// Pikachu's HP & Battle Info
 
-let PokemonGreeting = function(Pokemon, HP, powerMove, damage, potion) {
-    console.log(`${Pokemon} has ${HP} HP, their power move is ${powerMove}, it does ${damage} damage and they have a chance of healing by ${potion}%!`);
-};
+Pikachu = new Pokemon("Pikachu", 17, "Thundershock", 5, 2);
+myPokemon.attack(Pikachu);
 
-// if (pokemon = "Charmander") {
-//     console.log(Charmander);
-// } else if (pokemon = Bulbasaur) {
-//     console.log(Bulbasaur);
-// } else (pokemon = Squirtle) {
-//     console.log(Squirtle);
-// }
+// Battle Sequence
 
-let Pikachu = PokemonGreeting("Pikachu", 17, "Thunder Wave", 4, 2);
-let Charmander = PokemonGreeting("Charmander", 15, "Ember", 4, 1.5);
-let Bulbasaur = PokemonGreeting("Bulbasaur", 14, "Vine Whip", 4.5,2);
-let Squirtle = PokemonGreeting("Squirtle", 13, "Water Gun", 5, 3);
+// const battleSequence = [
+//     {
+//         choice: Charmander,
+//         options: ["Fight", "Potion", "Flee"]
+//     },
+//     {
+//         choice: Bulbasaur,
+//         options: ["Fight", "Potion", "Flee"]
+//     },
+//     {
+//         choice: Squirtle,
+//         options: ["Fight", "Potion", "Flee"]
+//     },
+//     {
+//         opponent: Pikachu,
+//         options: ["Fight", "Potion"]
+//     }
+// ]
 
+// Heal Sequence
 
-
-const battleSequence = [
-    {
-        choice: Charmander,
-        options: ["Fight", "Potion", "Flee"]
-    },
-    {
-        choice: Bulbasaur,
-        options: ["Fight", "Potion", "Flee"]
-    },
-    {
-        choice: Squirtle,
-        options: ["Fight", "Potion", "Flee"]
-    },
-    {
-        opponent: Pikachu,
-        options: ["Fight", "Potion"]
-    }
-]
 
 // Play Win Sequence
 
